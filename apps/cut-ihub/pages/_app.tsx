95de7d47f73c2e1c492b21dc7a9d7f463053ffba
tatendabakozw <tatendabakozw@gmail.com>
2023-05-02 12:52:05 +0200

    feature: added dashboard layout

--- apps/cut-ihub/pages/_app.tsx
+++ apps/cut-ihub/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import "./styles.css";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +10,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to cut-ihub!</title>
       </Head>
       <main className="app">
+        <ChakraProvider>
         <Component {...pageProps} />
+        </ChakraProvider>
       </main>
     </>
   );
