02e4c28cbcfb3eed442889b7c51fc6a593105cbc
tatendabakozw <tatendabakozw@gmail.com>
2023-06-07 12:44:53 +0200

    fix: edit news working

--- apps/dashboard/src/app/pages/news/EditNews.tsx
+++ apps/dashboard/src/app/pages/news/EditNews.tsx
@@ -80,10 +80,11 @@ const EditNews = (props: Props) => {
     }
   };
 
+
   const edit_news = async () => {
     setLoading(true);
     try {
-      const { data } = await axios.patch(`${apiUrl}/news/edit`, {
+      const { data } = await axios.patch(`${apiUrl}/news/edit/?id=${searchParams.get('id')}`, {
         heading,
         sub_heading,
         main_pic: url,
@@ -257,14 +258,23 @@ const EditNews = (props: Props) => {
                               />
                             </div>
                           ) : (
-                            <div className="mt-1 sm:mt-0 sm:col-span-2 flex flex-col w-full">
-                              <p>Uploading...</p>
-                              <Progress
-                                hasStripe
-                                size="sm"
-                                value={progress}
-                                className="w-full"
-                              />
+                            <div className="mt-1 sm:mt-0 sm:col-span-2">
+                              <div className="max-w-lg flex rounded-md shadow-sm">
+                                <div className="mb-3">
+                                  <label
+                                    htmlFor="formFile"
+                                    className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
+                                  >
+                                    Media
+                                  </label>
+                                  <input
+                                    className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                                    type="file"
+                                    onChange={on_picture_select}
+                                    id="formFile"
+                                  />
+                                </div>
+                              </div>
                             </div>
                           )}
                         </>
